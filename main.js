1711d8e35be7c01a941a425fcbb06a85baf5a688
Leonardo Matos <leomp120894@gmail.com>
2018-08-14 13:53:44 -0300

    initial js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-(function () {
+/**
+ * twbschema
+ * @author E-Com Club <ti@e-com.club>
+ * @license MIT
+ */
+
+window.twbschema = (function () {
   'use strict'
+
+  var doc = function () {
+    // main function
+    // convert JSON Schema to docs UI
+  }
+
+  // return methods
+  return {
+    'doc': doc
+  }
 }())
