05500b33e252657b97377269623da5c9886bce0f
Leonardo Matos <leomp120894@gmail.com>
2018-08-14 15:53:18 -0300

    parse object, add gen function

--- main.js
+++ main.js
@@ -7,9 +7,43 @@
 window.twbschema = (function () {
   'use strict'
 
-  var doc = function () {
+  var gen = function (json, html) {
+    // generate docs HTML
+    if (!html) {
+      // preset
+      html = ''
+    }
+    // merge all object properties
+    Object.assign(json, json.properties, json.patternProperties)
+
+    // returns rendered HTML
+    return html
+  }
+
+  var doc = function (element, schema) {
     // main function
-    // convert JSON Schema to docs UI
+    // receives DOM element and JSON Schema
+    // parse object first
+    var json, err
+    if (typeof schema === 'string') {
+      try {
+        json = JSON.parse(schema)
+      } catch (e) {
+        err = e
+      }
+    } else if (typeof schema === 'object' && schema !== null) {
+      json = schema
+    } else {
+      err = new Error('Schema must be a valid JSON object or string')
+    }
+
+    if (err) {
+      console.error('Fatal error: invalid schema', err)
+    } else {
+      // valid
+      // convert JSON Schema to docs UI
+      element.innerHTML = gen(json)
+    }
   }
 
   // return methods
