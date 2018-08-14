00dacbaa454cc511ef5006ee27fced182badd1e9
Leonardo Matos <leomp120894@gmail.com>
2018-08-14 19:16:57 -0300

    render row HTML (first release)

--- main.js
+++ main.js
@@ -31,8 +31,28 @@ window.twbschema = (function () {
     var props = Object.assign({}, json.properties, json.patternProperties)
 
     // check each object property
+    // one property per row
+    var rowBgClass = ''
     for (var field in props) {
       if (props.hasOwnProperty(field)) {
+        // stripped rows
+        if (rowBgClass !== '') {
+          rowBgClass = ''
+        } else {
+          // https://getbootstrap.com/docs/4.0/utilities/colors/
+          rowBgClass = ' bg-light'
+        }
+        var labelRequired = ''
+        if (Array.isArray(req)) {
+          for (var i = 0; i < req.length; i++) {
+            if (field === req[i]) {
+              // field is requred
+              labelRequired = '<div class="text-danger">* required</div>'
+              break
+            }
+          }
+        }
+
         var prop = props[field]
         var type = prop.type
         if (Array.isArray(type)) {
@@ -56,7 +76,21 @@ window.twbschema = (function () {
             break
         }
 
-        console.log(req, type, description)
+        // render row HTML
+        html += '<div class="row align-items-center border-bottom' + rowBgClass + '">' +
+                  '<div class="col-xs-12 col-4">' +
+                    '<div class="p-3">' +
+                      '<code>' + field + '</code>' +
+                      '<div class="small">' +
+                        '<span class="text-muted">' + type + '</span>' +
+                        labelRequired +
+                      '</div>' +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="col">' +
+                    '<div class="p-3">' + description + '</div>' +
+                  '</div>' +
+                '</div>'
       }
     }
 
@@ -87,7 +121,7 @@ window.twbschema = (function () {
       // valid
       // convert JSON Schema to docs UI
       var html = gen(json)
-      if (typeof el === 'object' && el !== null && el.hasOwnProperty('innerHTML')) {
+      if (typeof el === 'object' && el !== null && el.innerHTML) {
         el.innerHTML = html
       } else {
         // not a DOM element
